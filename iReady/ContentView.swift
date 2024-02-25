e7ddfa069139fb9737c43b66396054179074b3f0
Z00K00 <kraigalarson@gmail.com>
2024-02-25 18:49:36 -0500

    Initial Commit

---
+++ iReady/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iReady
+//
+//  Created by Krajg Larson on 2/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iReady/iReadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iReadyApp.swift
+//  iReady
+//
+//  Created by Krajg Larson on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct iReadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
